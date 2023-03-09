81620999292d2a2aecbe17472eb31691b89749ad
Ayo <typeman2022@outlook.com>
2023-03-09 14:25:38 -0600

    updated terraform code

--- instance1.tf
+++ instance1.tf
@@ -1,7 +1,13 @@
+resource "aws_key_pair" "newkeypair1" {
+  key_name   = "newkeypair1"
+  public_key = file("/home/ayoadmin/assignmtlast/keypair.pub")
+}
 resource "aws_instance" "web" {
   ami           = "ami-006dcf34c09e50022"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.kman.id
+  key_name = aws_key_pair.newkeypair1.key_name
+  associate_public_ip_address = true
 
   tags = {
     Name = "HelloWorld"
